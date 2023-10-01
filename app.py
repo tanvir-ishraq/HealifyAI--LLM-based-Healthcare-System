d059591590a7a271de9c942d2ade747aa7478651
tanvir-ishraq <72716721+tanvir-ishraq@users.noreply.github.com>
2023-10-01 14:52:10 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request #, redirect, url_for
 import json, pickle, requests
 import pandas as pd
 from numpy import argsort
